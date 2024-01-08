171fcf633cb71a4f7f772eef4e71805d3ad5a0a5
Fayis cm <cmf8034@gmail.com>
2024-01-08 09:34:00 +0530

    second commit

--- lib/application/feature/auth/view/main_page.dart
+++ lib/application/feature/auth/view/main_page.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import '../widget/heading.dart';
 import '../widget/sizedbox.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -18,34 +20,46 @@ class _RegisterPageState extends State<RegisterPage> {
             width: double.infinity,
             height: double.infinity,
             color: Colors.black,
-            child: Column(
-              children: [
-                const CustomSizedBox(
-                  hieght: 155,
-                ),
-                RichText(
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Connect\nfriends',
-                        style: TextStyle(color: Colors.white, fontSize: 45),
-                      ),
-                      TextSpan(
-                        text: '\neasily &\nquickly\n',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.w800),
-                      ),
-                      TextSpan(
-                        text:
-                            '\nOur chat app is the perfect way to stay \nconnected with friends and family.',
-                        style: TextStyle(
-                            color: Colors.grey, fontSize: 17, height: 1.4),
-                      ),
-                    ],
+             ),
+             Opacity(
+              opacity: .6,
+               child: Padding(
+                 padding: const EdgeInsets.only(right: 50,bottom: 320),
+                 child: Container(
+                             width: double.infinity,
+                             height: 650.0,
+                             decoration:const BoxDecoration(
+                  gradient: RadialGradient(
+                    center: Alignment.center,
+                    radius: .7,
+                    colors: [Color.fromARGB(255, 76, 34, 129), Colors.black],
                   ),
+                             ),
+                             ),
+               ),
+             ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            
+            const    CustomSizedBox(
+                  hieght: 50,
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "asset/images/splash.png",
+                      width: 55,
+                    ),
+                    const Text(
+                      'Chat App',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              const  HeadingText(),
+            
                 const CustomSizedBox(
                   hieght: 35,
                 ),
@@ -56,39 +70,89 @@ class _RegisterPageState extends State<RegisterPage> {
                       onTap: () {
                         print('clicked facebook');
                       },
-                      child: CircleAvatar(
-                          radius: 25,
-                          child: Image.asset('asset/images/face.webp')),
+                      child: Container(
+                        padding:const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
+                            border: Border.all(color: Colors.grey)),
+                        child: CircleAvatar(
+                            radius: 17,
+                            child: Image.asset('asset/images/face.webp')),
+                      ),
                     ),
                     const CustomSizedBox(
-                      hieght: 15,
+                      width: 15,
                     ),
                     InkWell(
                       onTap: () {},
-                      child: CircleAvatar(
-                          radius: 25,
-                          child: Image.asset('asset/images/google.png')),
+                      child: Container(
+                        padding:const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
+                            border: Border.all(color: Colors.grey)),
+                        child: CircleAvatar(
+                            radius: 17,
+                            child: Image.asset('asset/images/google.jpg')),
+                      ),
                     ),
                   ],
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 45, top: 25),
+                  padding: const EdgeInsets.only(left: 35, top: 25),
                   child: Row(
                     children: [
                       Container(
                         height: 2,
-                        width: 140,
-                        color: Colors.white,
+                        width: 172,
+                        color:const Color.fromARGB(255, 55, 55, 55),
+                      ),
+                      const CustomSizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'or',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      const CustomSizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 2,
+                        width: 172,
+                        color: const Color.fromARGB(255, 55, 55, 55),
                       )
                     ],
                   ),
                 ),
-                // Text("Connect\nfriends\neasily &\nquickly",style: TextStyle(color: Colors.white,fontSize: 35),),
+              const  CustomSizedBox(hieght: 35,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: Container(
+                    width: 385,
+                    height: 55,
+                    decoration:const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(15))
+                    ),
+                    child:const Center(child: Text("Sign up withn mail"),),
+                  ),
+                ),
+               const CustomSizedBox(hieght: 25,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   const Text('Existing account? ',style: TextStyle(color: Colors.grey,)),
+                    TextButton(onPressed: (){}, child:const Text('Log in',style: TextStyle(color: Colors.white),)),
+                  ],
+                )
               ],
             ),
-          )
+          
         ],
       ),
     );
   }
 }
+
---
+++ lib/application/feature/auth/widget/heading.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HeadingText extends StatelessWidget {
+  const HeadingText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 45),
+      child: RichText(
+        text: const TextSpan(
+          children: [
+            TextSpan(
+              text: 'Connect\nfriends',
+              style: TextStyle(color: Colors.white, fontSize: 75),
+            ),
+            TextSpan(
+              text: '\neasily &\nquickly\n',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 70.0,
+                  fontWeight: FontWeight.w800),
+            ),
+            TextSpan(
+              text:
+                  '\nOur chat app is the perfect way to stay \nconnected with friends and family.',
+              style: TextStyle(
+                  color: Colors.grey, fontSize: 19, height: 1.4),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
