beea39cc943cacc07f78d8f279e0566795e3d4f4
Fayis cm <cmf8034@gmail.com>
2024-02-19 12:36:48 +0530

    imagefulldisplay

--- lib/application/feature/personalData/widget/singlemessage.dart
+++ lib/application/feature/personalData/widget/singlemessage.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/application/feature/personalData/widget/showimage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -25,75 +26,96 @@ class SingleMessage extends StatelessWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-             type=='text'?   Container(
-                    padding: const EdgeInsets.all(10),
-                    margin: const EdgeInsets.only(
-                        top: 10, bottom: 2, left: 10, right: 10),
-                    constraints: const BoxConstraints(maxWidth: 200),
-                    decoration: BoxDecoration(
-                        color: isMe
-                            ? const Color(0xffADD8E6)
-                            : Color.fromARGB(255, 234, 242, 248),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(15),
-                        )),
-                    child: Text(
-                      message,
-                      style: TextStyle(
-                          fontSize: 17,
-                          color: isMe ? Colors.white : Colors.black),
-                    )):Container(
-                margin: const EdgeInsets.only(right: 20, top: 10),
-                child:type == "link"
-                ? Container(
-                    padding: const EdgeInsets.all(16),
-                    margin: const EdgeInsets.all(16),
-                    constraints: const BoxConstraints(maxWidth: 200),
-                    decoration: BoxDecoration(
-                        color: isMe ? Colors.black : Colors.grey,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(12))),
-                    child: GestureDetector(
-                      onTap: () async {
-                        await launchUrl(Uri.parse('$message'));
-                      },
-                      child: Text(
-                        message,
-                        style: const TextStyle(
-                          fontStyle: FontStyle.italic,
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ):
-                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.42,
-                          height: MediaQuery.of(context).size.height * 0.30,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black),
-                              borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(18),
-                                topLeft: Radius.circular(18),
-                                bottomLeft: Radius.circular(18),
+                type == 'text'
+                    ? Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.only(
+                            top: 10, bottom: 2, left: 10, right: 10),
+                        constraints: const BoxConstraints(maxWidth: 200),
+                        decoration: BoxDecoration(
+                            color: isMe
+                                ? const Color(0xffADD8E6)
+                                : Color.fromARGB(255, 234, 242, 248),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              bottomLeft: Radius.circular(12),
+                              bottomRight: Radius.circular(15),
+                            )),
+                        child: Text(
+                          message,
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: isMe ? Colors.white : Colors.black),
+                        ))
+                    : Container(
+                        margin: const EdgeInsets.only(right: 20, top: 10),
+                        child: type == "link"
+                            ? Container(
+                                padding: const EdgeInsets.all(16),
+                                margin: const EdgeInsets.all(16),
+                                constraints:
+                                    const BoxConstraints(maxWidth: 200),
+                                decoration: BoxDecoration(
+                                    color: isMe ? Colors.black : Colors.grey,
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(12))),
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    await launchUrl(Uri.parse('$message'));
+                                  },
+                                  child: Text(
+                                    message,
+                                    style: const TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      InkWell(
+                                        onTap: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      ShowImage(
+                                                          imageUrl: message)));
+                                        },
+                                        child: Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.42,
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.30,
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.black),
+                                              borderRadius:
+                                                  const BorderRadius.only(
+                                                topRight: Radius.circular(18),
+                                                topLeft: Radius.circular(18),
+                                                bottomLeft: Radius.circular(18),
+                                              ),
+                                              image: DecorationImage(
+                                                  image: NetworkImage(message),
+                                                  fit: BoxFit.fill)),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
                               ),
-                              image: DecorationImage(
-                                  image: NetworkImage(message),
-                                  fit: BoxFit.fill)),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-               ),
+                      ),
                 Padding(
                   padding: const EdgeInsets.only(right: 17),
                   child: Text(
